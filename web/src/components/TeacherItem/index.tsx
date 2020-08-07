48ef686bfe03eca8e5fab8cf0028e554193c57fe
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2020-08-06 22:43:08 -0300

    Add: finished day 03

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,35 +2,56 @@ import React from "react";
 
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import "./styles.css"
+import "./styles.css";
+import api from "../../services/api";
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    user_id: number;
+    whatsapp: string;
+};
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/25934151?s=460&u=07de59de0364061d025d16ce8beda2274f3d4f46&v=4" alt="Vinicius"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Vinicius Spada Melo</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. In itaque iusto magnam mollitia nemo
-                <br/><br/>
-                nihil perferendis quaerat sequi sunt vel. Aliquid animi eveniet fugiat in laudantium molestias
-                non odit vero.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`http://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Wpp icon"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom"
 
 import logo from "../../assets/images/logo.svg"
@@ -8,8 +8,19 @@ import giveClassesIcon from "../../assets/images/icons/give-classes.svg"
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg"
 
 import "./styles.css"
+import api from "../../services/api";
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        });
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -26,13 +37,13 @@ function Landing() {
                         Estudar
                     </Link>
                     <Link to="/give-classes" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                        Estudar
+                        <img src={giveClassesIcon} alt="Dar aulas"/>
+                        Dar aulas
                     </Link>
                 </div>
 
                 <div className="total-connections">
-                    Total de 200 conexões já realizadas
+                    Total de {totalConnections} conexões já realizadas
                     <img src={purpleHeartIcon} alt="Coração roxo"/>
                 </div>
             </div>
